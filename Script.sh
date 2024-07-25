4ffd0f95d7fd44963cea64282dd52825f6fc8103
devinamua92 <148983521+devinamua92@users.noreply.github.com>
2024-07-25 11:06:18 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -7,6 +7,7 @@ echo "Checking uptime..." >> ./output_log.txt
 echo $(uptime) >> ./output_log.txt
 
 echo "THIS IS MY UPDATED SCRIPT" >> ./output_log.txt
-echo "THIS IS MY NEW CODE" >> ./output_log.txtn
+echo "THIS IS MY NEW CODE" >> ./output_log.txt
+echo "THIS IS MY TEST" >>./output_log.txt
 
 echo "CDCI process completed successfully."
